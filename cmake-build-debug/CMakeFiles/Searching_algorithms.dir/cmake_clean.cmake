a90dd895d811408217681bef6b3d541691d58429
Monakus <monakus00@gmail.com>
2022-02-01 14:34:19 +0100

    Initial commit.

---
+++ cmake-build-debug/CMakeFiles/Searching_algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Searching_algorithms.dir/src/main.cpp.obj"
+  "Searching_algorithms.exe"
+  "Searching_algorithms.exe.manifest"
+  "Searching_algorithms.pdb"
+  "libSearching_algorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Searching_algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Studenkie Projekty/CPP/Searching_algorithms/Google_tests/searchingTests.cpp" "D:/Studenkie Projekty/CPP/Searching_algorithms/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/searchingTests.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Google_tests/../include"
+  "../Google_tests/lib/googletest/include"
+  "../Google_tests/lib/googletest"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "D:/Studenkie Projekty/CPP/Searching_algorithms/cmake-build-debug/Google_tests/lib/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "D:/Studenkie Projekty/CPP/Searching_algorithms/cmake-build-debug/Google_tests/lib/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
