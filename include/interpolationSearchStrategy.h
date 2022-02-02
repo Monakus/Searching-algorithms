27dff0ff746b9e6d1039862a5f437177343c09ab
Monakus <monakus00@gmail.com>
2022-02-02 14:36:30 +0100

    Refactored Interpolation Search.

--- include/interpolationSearchStrategy.h
+++ include/interpolationSearchStrategy.h
@@ -5,8 +5,14 @@
 
 class InterpolationSearchStrategy : public SearchingStrategy{
 public:
-    int search(const int *array, int count, int toFind) override;
+    int search(const int array[], int count, int toFind) override;
 private:
+    int leftBoundary;
+    int rightBoundary;
+    bool checkIfInBoundaries(const int array[], int toFind);
+    int checkIfValueExists(const int array[], int toFind);
+    int calculateIndex(const int array[], int toFind);
+    void updateBoundaries(int valueAtCurrentIndex, int index, int toFind);
 };
 
 #endif //SEARCHING_ALGORITHMS_INTERPOLATIONSEARCHSTRATEGY_H
--- src/interpolationSearchStrategy.cpp
+++ src/interpolationSearchStrategy.cpp
@@ -1,23 +1,39 @@
 #include "../include/interpolationSearchStrategy.h"
 
-// TO DO: Refactor
 int InterpolationSearchStrategy::search(const int *array, int count, int toFind) {
-    int leftBoundary = 0;
-    int rightBoundary = count - 1;
-    while(leftBoundary <= rightBoundary && toFind >= array[leftBoundary] && toFind <= array[rightBoundary]){
-        if(leftBoundary == rightBoundary){
-            if(isFound(array[leftBoundary], toFind))
-                return leftBoundary;
-            return -1;
-        }
-        int index = leftBoundary + (((double)(rightBoundary - leftBoundary) / (array[rightBoundary] - array[leftBoundary])) * (toFind - array[leftBoundary]));
+    leftBoundary = 0;
+    rightBoundary = count - 1;
+    while(checkIfInBoundaries(array, toFind)){
+        if(leftBoundary == rightBoundary)
+            return checkIfValueExists(array, toFind);
+
+        int index = calculateIndex(array, toFind);
 
         if(isFound(array[index], toFind))
             return index;
-        if(array[index] < toFind)
-            leftBoundary = index + 1;
-        else
-            rightBoundary = index - 1;
+
+        updateBoundaries(array[index], index, toFind);
     }
     return -1;
+}
+
+bool InterpolationSearchStrategy::checkIfInBoundaries(const int array[], int toFind) {
+    return (leftBoundary <= rightBoundary && toFind >= array[leftBoundary] && toFind <= array[rightBoundary]);
+}
+
+int InterpolationSearchStrategy::checkIfValueExists(const int array[], int toFind) {
+    if(isFound(array[leftBoundary], toFind))
+        return leftBoundary;
+    return -1;
+}
+
+int InterpolationSearchStrategy::calculateIndex(const int array[], int toFind) {
+    return int(leftBoundary + (((double)(rightBoundary - leftBoundary) / (array[rightBoundary] - array[leftBoundary])) * (toFind - array[leftBoundary])));
+}
+
+void InterpolationSearchStrategy::updateBoundaries(int valueAtCurrentIndex, int index, int toFind) {
+    if(valueAtCurrentIndex < toFind)
+        leftBoundary = index + 1;
+    else
+        rightBoundary = index - 1;
 }
